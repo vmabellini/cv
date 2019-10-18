09dd51419720afe8934aad79f483c6ebc605ce4c
Vinicius Mabellini <vpmabellini@gmail.com>
2019-10-18 10:47:20 -0300

    Removing unused files and adding material-ui dependencies

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Button from "@material-ui/core/Button";
 
 function App() {
   return (
     <div className="App">
+      <Button variant="contained" color="primary">
+        Hello World
+      </Button>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
