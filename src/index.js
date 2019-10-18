3d1a132bb256b5487319bd4bbfead871c6468bc8
Vinicius Mabellini <vpmabellini@gmail.com>
2019-10-18 11:21:31 -0300

    adding material bootstrap because I don't know how to use it yet

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { ThemeProvider } from "@material-ui/styles";
 import App from "./App";
+import theme from "./theme";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+    <CssBaseline />
+    <App />
+  </ThemeProvider>,
+  document.getElementById("root")
+);
